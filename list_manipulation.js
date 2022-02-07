01ad7832dbf400f3d0385faaec65ed7e8c78c44e
Daniel Coimbra <coimbrasantos.d@gmail.com>
2022-02-07 18:55:52 -0300

    List manipulation function. Sorts the list and then removes any duplicates.

---
+++ list_manipulation.js
@@ -0,0 +1,8 @@
+var remove_duplicate = (list) => {
+    list.sort();
+    return list.filter((value, index)=>{
+        return list.indexOf(value) === index;
+    });
+}
+
+module.exports = { remove_duplicate }
