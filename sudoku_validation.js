0f41a05d68736b5535342a514042ef4fcc5c9d9a
Daniel Coimbra <coimbrasantos.d@gmail.com>
2022-02-07 19:01:47 -0300

    Add function to check the boxes in a board

--- sudoku_validation.js
+++ sudoku_validation.js
@@ -8,3 +8,41 @@ var list_valid = (list) => {
     }
     return false;
 };
+
+var reset_list = () => {
+    return {
+        1:false,
+        2:false,
+        3:false,
+        4:false,
+        5:false,
+        6:false,
+        7:false,
+        8:false,
+        9:false,
+    }
+}
+
+var box_bounds = [[0,0],[0,3],[0,6],[3,0],[3,3],[3,6],[6,0],[6,3],[6,6]];
+
+var box_check = (board)=>{
+    for(var i=0;i<9;i++){
+        var num_list = reset_list();
+        b_x = box_bounds[i][0], b_y = box_bounds[i][1];
+
+        for(var x=b_x;x<=b_x+2;x++){
+            for(var y=b_y;y<=b_y+2;y++){
+
+                if(board[x][y]===".")
+                    continue;
+                if(num_list[board[x][y]] === false){
+                    num_list[board[x][y]] = true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
